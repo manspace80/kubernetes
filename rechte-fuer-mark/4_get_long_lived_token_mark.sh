242ad8d7b67d3c1adffc924f5ea62a7e58dce5c4
Mathias Meyer <mm@compuclick.de>
2024-09-26 13:22:21 +0000

    variablen angepasst

--- rechte-fuer-mark/4_get_long_lived_token_mark.sh
+++ rechte-fuer-mark/4_get_long_lived_token_mark.sh
@@ -9,9 +9,9 @@ do
 	echo "${TOKEN}"
 	#echo "${TOKEN}" > ${DATADIR}/${U}_long_live.token
 	/usr/bin/cp ${HOME}/.kube/config ${HOME}/.kube/config-mark
-	kubectl config set-context super-mark --cluster default --user mark --kubeconfig ${HOME}/.kube/config-mark
-	kubectl config set-credentials mark --token=${TOKEN} --kubeconfig ${HOME}/.kube/config-mark
-	kubectl config use-context super-mark --kubeconfig ${HOME}/.kube/config-mark
+	kubectl config set-context super-${U} --cluster default --user ${U} --kubeconfig ${HOME}/.kube/config-mark
+	kubectl config set-credentials ${U} --token=${TOKEN} --kubeconfig ${HOME}/.kube/config-mark
+	kubectl config use-context super-${U} --kubeconfig ${HOME}/.kube/config-mark
 	kubectl config delete-context default --kubeconfig ${HOME}/.kube/config-mark
 	kubectl config delete-user default --kubeconfig ${HOME}/.kube/config-mark
 done
